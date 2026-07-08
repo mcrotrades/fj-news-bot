bd3867c577d1f471b6b6e01dc934b43230ca5b2b
mcrotrades <manandicromedick@gmail.com>
2026-07-08 15:50:17 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ CHANNEL_COT = os.environ.get("CHANNEL_COT", "-1004370012411")
 CHANNEL_MCR = os.environ.get("CHANNEL_MCR", "-1004370012411")
 
 GROQ_API_KEY   = os.environ.get("GROQ_API_KEY", "")
-GEMINI_API_KEY = ""
+GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY", "")
 
 FJ_POLL_INTERVAL   = int(os.environ.get("FJ_POLL_INTERVAL",   90))
 ECON_POLL_INTERVAL = int(os.environ.get("ECON_POLL_INTERVAL", 300))
